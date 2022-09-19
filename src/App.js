fdf4db759eaaac4b0eaafbb09fdefd8b0183fd9f
Rasel <mdraselm413@gmail.com>
2022-09-20 00:09:05 +0600

    person

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 5555;
+const singer = {name: 'Dr. Mahfuzur Rahman', job: 'Singer' };
+const singer2 = {name: 'Eva Rahman', job: 'Singer2'};
+const singerStyle = {
+  color: 'purple',
+  backgroundColor: 'white'
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
     </div>
   );
 }
+
+function Person(){
+  return (
+    <div>
+      <h1>Sakib Al Hasan</h1>;
+      <p>Profession: Cricket</p>;
+  </div>
+  )
+}
+
 
 export default App;
